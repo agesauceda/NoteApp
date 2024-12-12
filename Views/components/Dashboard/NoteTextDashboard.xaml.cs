276f0f09642f087704e416bb6101b424a9fc8096
agoseguera <163493757+agoseguera@users.noreply.github.com>
2024-12-11 23:09:22 -0600

    TextPageDelete

--- Views/components/Dashboard/NoteTextDashboard.xaml.cs
+++ Views/components/Dashboard/NoteTextDashboard.xaml.cs
@@ -1,6 +1,8 @@
 using Microsoft.Maui.Controls;
+using NoteApp.Models.common;
 using NoteApp.Models.Dashboard;
 using NoteApp.Models.TextPage;
+using NoteApp.Services.TextPage;
 
 namespace NoteApp.Views.components.Dashboard;
 
@@ -8,12 +10,14 @@ public partial class NoteTextDashboard : ContentView
 {
 
     public ObjectDashBoard element { get; set; }
+    private TextPageDelete service;
 
     public event EventHandler<int> NoteClicked;
     public NoteTextDashboard(ObjectDashBoard e) 
 	{
 		InitializeComponent();
 		element = e;
+        service = new TextPageDelete();
 		InitComponent();
 	}
 	private void InitComponent() {
@@ -24,5 +28,11 @@ public partial class NoteTextDashboard : ContentView
     public async void OnEditClicked(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new TextPageUpdate(element));
+    }
+
+    public async void OnDeleteClicked(object sender, EventArgs e)
+    {
+        ApiResponse response = await service.DeleteNoteText(element.id.Value);
+        await Application.Current.MainPage.DisplayAlert("Eliminacíón de Nota", (response.status.Value) ? response.message : "No se pudo eliminar la nota", "Aceptar");
     }
 }
